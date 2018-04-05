92583287690a54f0802565740dcd72de1b256f97
Juan Borrajo Rodriguez <juanborrajo10@gmail.com>
2018-04-05 10:14:49 +0200

    Menú realizado parcialmente

--- src/main/java/com/juan/pruebaclonadocommitpush/Principal.java
+++ src/main/java/com/juan/pruebaclonadocommitpush/Principal.java
@@ -3,6 +3,8 @@ package com.juan.pruebaclonadocommitpush;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.kohsuke.github.GHCreateRepositoryBuilder;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
@@ -12,14 +14,35 @@ import org.kohsuke.github.GitHub;
 public class Principal {
 
     public static void main(String[] args) {
-        try {
-            GitHub github = GitHub.connect();
-            GHRepository repo = github.createRepository(
-                    "Repo creado desde netbeans", "Repositorio de prueba de la API de GitHub",
-                    " ", true/*public*/);
-        } catch (IOException ex) {
-            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        int x;
+        do {
+            Funciones.credentials();
+            x = Integer.parseInt(JOptionPane.showInputDialog("Pulsa 1 Para crear repositorio"));
+            switch (x) {
+                
+                case 1: {
+                    try {
+                        Funciones.nombreRepo();
+                        Funciones.desc();
+                        GitHub github = GitHub.connectUsingPassword(Funciones.getUser(),Funciones.getPass());
+                        GHRepository builder = github.createRepository(Funciones.getNombreR(),Funciones.getDesc()," ", true);
+                    } catch (IOException ex) {
+                        Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+                break;
+                case 2:{
+                    
+                    
+                 break;   
+                }
+                default:
+                    throw new AssertionError();
+            }
+
+        } while (x == 0);
+
     }
 
 }
